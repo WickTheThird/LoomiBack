25b5910a0c9c3535851b195d10b902ca9306514b
Filip-Bumbu <filipb@telnyx.com>
2025-12-18 12:59:38 +0200

    health check

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
-fn main() {
+mod routes;
 
-    
+#[tokio::main]
+async fn main() {
+    let app: axum::Router = routes::router();
+    let address = "0.0.0.0:3000"; // TODO -> place this inside a config/env file
+
+    let listener = tokio::net::TcpListener::bind(address)
+        .await
+        .expect("Failed to bind to address");
+
+    println!("Listening on address: {}", address);
+
+    axum::serve(listener, app)
+        .await
+        .expect("server failed");
+
 }
---
+++ src/routes.rs
@@ -0,0 +1,16 @@
+use axum::{routing::get, Json, Router};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct HealthResponse {
+    status: &'static str,
+}
+
+async fn health() -> Json<HealthResponse> {
+    Json(HealthResponse { status: "ok" })
+}
+
+pub fn router() -> Router {
+    Router::new().route("/health", get(health))
+}
+    
